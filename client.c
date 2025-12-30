d2b84dcb376a785d7b5782b815672c7fed1d4281
Maria Camila Pedraza Casasbuenas <mpedraza@f2r12s11.clusters.42paris.fr>
2025-12-30 17:57:48 +0100

    working with basic ack - needs per message ack

--- client.c
+++ client.c
@@ -6,22 +6,24 @@
 /*   By: mpedraza <mpedraza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/28 17:17:16 by mpedraza          #+#    #+#             */
-/*   Updated: 2025/12/28 23:17:48 by mpedraza         ###   ########.fr       */
+/*   Updated: 2025/12/30 17:47:20 by mpedraza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// Client Checklist
-// main program takes two parameters (PID of server, message)
-// it encrypts the message (via bits)?
-// sends the message to the server (via bits)?
-// create a stop condition so the server knows the message is over (a stop codon?)
-
 #include "minitalk.h"
 
-static int ft_atoi(const char *nptr)
+volatile sig_atomic_t	g_acknowledged = 0;
+
+void	ack_handler(int sig)
 {
-	int val;
-	int n;
+	(void)sig;
+	g_acknowledged = 1;
+}
+
+static int	ft_atoi(const char *nptr)
+{
+	int	val;
+	int	n;
 
 	val = 1;
 	n = 0;
@@ -41,36 +43,58 @@ static int ft_atoi(const char *nptr)
 	return (n * val);
 }
 
-static void send_char(pid_t pid, char ch)
+static void	send_char(pid_t pid, char ch)
 {
-	int i;
-	
+	int	i;
+	int	elapsed;
+
 	i = 0;
 	while (i < 8)
 	{
-		if (ch & (1 << i)) 
-			kill(pid, SIGUSR1); // send 1
+		elapsed = 0;
+		if (ch & (1 << i))
+			kill(pid, SIGUSR1);
 		else
-			kill(pid, SIGUSR2); // send 0
-		usleep(500);
+			kill(pid, SIGUSR2);
+		while (!g_acknowledged && elapsed < ACK_TIMEOUT)
+		{
+			usleep(ACK_INTERVAL);
+			elapsed += ACK_INTERVAL;
+		}
+		if (!g_acknowledged)
+		{
+			write(2, "Server Error ʕノ•ᴥ•ʔノ ︵ ┻━┻ \n", 48);
+			exit(EXIT_FAILURE);
+		}
+		g_acknowledged = 0;
 		i++;
 	}
+	g_acknowledged = 0;
 }
 
-static void send_msg(pid_t pid, char *str)
+static void	send_msg(pid_t pid, char *str)
 {
-	int i = 0;
+	int	i;
+
+	i = 0;
 	while (str[i])
 	{
 		send_char(pid, str[i]);
 		i++;
 	}
 	send_char(pid, '\0');
+	write(1, "ʕっ•ᴥ•ʔっ Message Delivered\n", 38);
 }
 
 int	main(int ac, char **av)
 {
-	int	pid;
+	pid_t				pid;
+	struct sigaction	handshake;
+
+	handshake.sa_flags = SA_RESTART | SA_SIGINFO;
+	sigemptyset(&handshake.sa_mask);
+	handshake.sa_handler = ack_handler;
+	sigaction(SIGUSR1, &handshake, NULL);
 	if (ac != 3)
 		exit(EXIT_SUCCESS);
 	pid = ft_atoi(av[1]);
